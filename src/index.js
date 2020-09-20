5bd53a211a6ae0ef645492ae60a9df581acf660e
va-z <vasilizadorojnyuk@gmail.com>
2020-09-20 22:18:09 +0500

    feat: solve index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,50 @@
-module.exports = function toReadable (number) {
-  
-}
+module.exports = function toReadable(number) {
+    const DICT_0_20 = [
+        "zero",
+        "one",
+        "two",
+        "three",
+        "four",
+        "five",
+        "six",
+        "seven",
+        "eight",
+        "nine",
+        "ten",
+        "eleven",
+        "twelve",
+        "thirteen",
+        "fourteen",
+        "fifteen",
+        "sixteen",
+        "seventeen",
+        "eighteen",
+        "nineteen",
+        "twenty",
+    ];
+    const DICT_20_100 = [
+        null,
+        null,
+        "twenty",
+        "thirty",
+        "forty",
+        "fifty",
+        "sixty",
+        "seventy",
+        "eighty",
+        "ninety",
+    ];
+
+    if (number <= 20) return DICT_0_20[number];
+    else if (number < 100) {
+        const ten = Math.floor(number / 10);
+        const one = number % 10;
+        return DICT_20_100[ten] + (one ? " " + DICT_0_20[one] : "");
+    } else {
+        const hundred = Math.floor(number / 100);
+        const ten = number % 100;
+        return (
+            DICT_0_20[hundred] + " hundred" + (ten ? " " + toReadable(ten) : "")
+        );
+    }
+};
